428f4c1741528faa7e6c106e3678284588e7e218
azharutama <azharutama837@gmail.com>
2024-10-13 14:55:40 +0700

    VIDI0 4 MVC - Controller

--- MVC/app/core/App.php
+++ MVC/app/core/App.php
@@ -2,10 +2,42 @@
 class App
 {
 
+  protected $controller = 'Home';
+  protected $method = 'index';
+  protected $params = [];
+
   public function __construct()
   {
+
     $url = $this->parseURL();
-    var_dump($url);
+
+    //controller
+    if ($url && file_exists('../app/controllers/' . $url[0] . '.php')) {
+
+      $this->controller = $url[0];
+      unset($url[0]);
+    }
+
+    require_once '../app/controllers/' . $this->controller . '.php';
+    $this->controller = new $this->controller;
+
+    if (isset($url[1])) {
+      if (method_exists($this->controller, $url[1])) {
+        $this->method = $url[1];
+        unset($url[1]);
+      }
+    }
+
+    $this->params = $url ? array_values($url) : [];
+
+    //jalankan params dan methodca
+    call_user_func_array([$this->controller, $this->method], $this->params);
+
+
+    //params
+    if (!empty($url[1])) {
+      $this->params = array_values($url);
+    }
   }
 
   public function parseURL()
